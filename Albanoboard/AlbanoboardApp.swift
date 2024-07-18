a1d32abafd6fa64f85f3d266971759c9fc57aa54
Antonio Solida <solida.antonio@gmail.com>
2024-07-18 16:07:23 +0200

    Initial Commit

---
+++ Albanoboard/AlbanoboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbanoboardApp.swift
+//  Albanoboard
+//
+//  Created by Antonio Solida on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AlbanoboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
