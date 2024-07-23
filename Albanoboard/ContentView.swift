7f164ea7933fbe48eeda469292e149b110cdfcfc
Antonio Solida <solida.antonio@gmail.com>
2024-07-23 12:46:48 +0200

    Add picker

--- Albanoboard/ContentView.swift
+++ Albanoboard/ContentView.swift
@@ -6,19 +6,147 @@
 //
 
 import SwiftUI
+import AVKit
+
+class SoundManager: ObservableObject {
+    var player: AVAudioPlayer?
+
+    func playSound(sound: String, type: String) {
+        guard let url = Bundle.main.url(forResource: sound, withExtension: type) else {
+            print("File audio non trovato.")
+            return
+        }
+        print("Audio trovato")
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            //player?.volume = 1.0  // Imposta il volume al 100%
+            player?.prepareToPlay()
+            print("Riproduzione")
+            player?.play()
+        } catch {
+            print("Errore durante la riproduzione del file audio:", error.localizedDescription)
+        }
+    }
+}
+
+
+/*func playSound(sound: String, type: String) {
+    guard let url = Bundle.main.url(forResource: sound, withExtension: type) else {
+            print("File audio non trovato.")
+            return
+        }
+        print("Audio trovato")
+        do {
+            let player = try AVAudioPlayer(contentsOf: url)
+            player.volume = 1.0  // Imposta il volume al 100%
+            player.prepareToPlay()
+            print("Riproduzione")
+            player.play()
+        } catch {
+            print("Errore durante la riproduzione del file audio:", error.localizedDescription)
+        }
+}*/
+
 
 struct ContentView: View {
+    @StateObject private var soundManager = SoundManager()
+    @State private var selectedMode: Set<String> = []
+    @State private var defaultMode: String = "1"
+    
+    private let sounds = [
+            "1",
+            "2",
+            "3"
+        ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Picker("", selection: $defaultMode) {
+                            ForEach(sounds, id: \.self) { sound in
+                                Text(sound).tag(sound)
+                            }
+                        }
+            .pickerStyle(.segmented)
+                        .padding()
+            HStack {
+                Button(action: {
+                    //print("Button 1 tapped")
+                    soundManager.playSound(sound: "I delfini", type: "mp3")
+                }) {
+                    Text("I delfini")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                Button(action: {
+                    soundManager.playSound(sound: "Gli elefanti", type: "mp3")
+                }) {
+                    Text("Gli elefanti")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            .padding(.bottom, 10)
+            
+            HStack {
+                Button(action: {
+                    soundManager.playSound(sound: "Le nuvole", type: "mp3")
+                }) {
+                    Text("Le nuvole")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                Button(action: {
+                    soundManager.playSound(sound: "I treni", type: "mp3")
+                }) {
+                    Text("I treni")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            .padding(.bottom, 10)
+            
+            HStack {
+                Button(action: {
+                    soundManager.playSound(sound: "E tu", type: "mp3")
+                }) {
+                    Text("E tu")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                Button(action: {
+                    soundManager.playSound(sound: "Dove vai a ballare", type: "mp3")
+                }) {
+                    Text("Dove vai a ballare?")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
         }
+        .frame(width: 300, height: 200)
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
